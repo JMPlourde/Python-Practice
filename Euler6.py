58e54cabf643089f7fde1c3b8a23931f51a75f81
JMPlourde <jessica.marie.plourde@gmail.com>
2018-01-30 18:13:30 -0500

    Update Euler6.py

--- Euler6.py
+++ Euler6.py
@@ -1,17 +1,18 @@
 #Find the difference between the sum of the squares of the first one 
 # hundred natural numbers and the square of the sum.
 
-sumsquares = 0
 def difference(x):
-    def sumsquare(x):
+    def sumofsquares(x):
+        sumsquares = 0
         for i in range(1,x):
             sumsquares += i**2
+        return sumsquares
     def squareofsum(x):
         a = x
         b = 0
         while a>0:
             b += a
             a -= 1
-        return b**2
-    return sumsquares
+        return b
+    return sumofsquares(x) - squareofsum(x)
 print(difference(100))
