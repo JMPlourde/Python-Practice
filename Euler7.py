07724e2668c7bccec6e25ef411224ae017df2cc4
JMPlourde <jessica.marie.plourde@gmail.com>
2018-01-30 22:16:29 -0500

    Update Euler7.py

--- Euler7.py
+++ Euler7.py
@@ -1,16 +1,22 @@
 #What is the 10 001st prime number?
-def prime(y):
-    while x <=y:
-        x = 1
-        primes = {}:
-        primes[x] = primetest()
-        x+=1
-    print(primes(y))
 
-def primetest(n):
+def primetest(a):
     y = 2
-    while y*y <= n:
-        if n % y == 0:
+    while y*y <= a:
+        if a % y == 0:
             return False
         y+=1
-    return n
+    return True
+
+def findprime(n):
+    primefac = []
+    x = 2
+    count = 0
+    while count < n:
+        if primetest(x):
+            primefac.append(x)
+            count += 1
+        x+=1
+    return max(primefac)
+print(findprime(6))
+print(findprime(10001))
