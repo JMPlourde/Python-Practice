a2b658ef0bff09bdc86d846cf3c50d9ef65b6831
JMPlourde <jessica.marie.plourde@gmail.com>
2018-02-12 01:22:50 -0500

    Update Euler12.py

--- Euler12.py
+++ Euler12.py
@@ -1,27 +1,22 @@
-      
+from math import sqrt
 def trinum(x):
-    addthings = lambda a,b: a+b
     z = 1 #length of bottom row of dots
     y = 0 #total dots
     factors = 0 #number of factors
     while factors<x:
         factors = 0
         n = 1
-        while n<=y:
-            if y%64==0 and y%3==0 and y%5==0 and y%7==0 and y%11==0:
-                if y%n==0:
-                    factors+=1
-                    n+=1
-                else:
-                    n+=1
+        while n<=int(sqrt(y)):
+            if y%n==0:
+                factors+=2
+                n+=1
             else:
                 n+=1
-        print("botdots", z)
-        print("dots", y)
-        print("factors",factors)
+        n = 1
         if factors<x:
             y=y+z
-        n = 1
-        z += 1
+            z += 1        
     return y
-print(trinum(500))       
+print(trinum(500))  
+
+            
