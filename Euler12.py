87a6a3191a60ef9f25c604b11f194bfcf3c6b83b
JMPlourde <jessica.marie.plourde@gmail.com>
2018-02-13 09:50:56 -0500

    Update Euler12.py

--- Euler12.py
+++ Euler12.py
@@ -1,24 +1,21 @@
 from math import sqrt
 
-def numoffacs(a,b):
+def numoffacs(b): #finds number of factors in each (b)
     factors = 0 #number of factors
-    while factors<a:
-        factors = 0
-        n = 1
-        while n<=int(sqrt(b)):
-            if b%n==0:
-                factors+=2
-            n+=1
-        n = 1
-        return factors        
+    n = 1
+    while n<=int(sqrt(b)):
+        if b%n==0:
+            factors+=2
+        n+=1
+    n = 1
+    return factors        
 
-def trinum(x):
-    z = 1 #length of bottom row of dots
-    y = 0 #total dots  
-    while numoffacs(x,y)<x:
-        y=y+z
-        z += 1
-        print(y)
-    return y
-print(trinum(500))  
-        
+def trinum(x): #increases total number of units y until y is over (x)
+   z = 1 #length of bottom row of dots
+   y = 0 #total dots  
+   while numoffacs(y)<x:
+       y=y+z
+       z += 1
+       print(y)
+   return y
+print(trinum(500))
